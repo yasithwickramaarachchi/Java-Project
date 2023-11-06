e5bf4b7a17a829318487891534d1b66917b099ed
yasithwickramaarachchi <138301307+yasithwickramaarachchi@users.noreply.github.com>
2023-11-06 22:17:29 +0530

    Add files via upload

---
+++ DashBord/sellerupdate/src/main/java/com/mycompany/sellerupdate/Sellerupdate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sellerupdate;
+
+/**
+ *
+ * @author Yasith
+ */
+public class Sellerupdate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
